0df9ce1bac7f0c1c657446b583b9e42e7353c02b
Renato <renato.silva15948@gmail.com>
2022-05-15 17:56:26 -0300

    exercicios_prova

---
+++ Fatorial/Fatorial.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercícios_P1
+{
+    internal class Fatorial
+    {
+        static void Main(string[] args)
+        {
+            /*4) faça o fatorial de um número fonecido pelo usuário
+            então apresente na tela somente os pares*/
+
+            int num, fatorial, cont;
+            num = 0;
+            fatorial = 1;
+
+            Console.WriteLine("Digite um número");
+            num = Convert.ToInt16(Console.ReadLine());
+
+            if (num < 0)
+            {
+                Console.WriteLine("número inválido");
+            }
+            else if ((num == 0) || (num == 1))
+            {
+                Console.WriteLine("Fatorial de {0} é igual a:", num);
+            }
+            else
+            {                               //cont = cont - 1
+                for (cont = num; cont >= 1; cont--)
+                {
+                    //fatorial = fatorial * cont;
+                    /* a variável fatorial armazena e multiplica o valor de cont, 
+                    assim vai acumulando os resultados*/
+                    fatorial *= cont;
+                }
+                if(fatorial % 2 == 0)
+                {
+                    Console.WriteLine($"O fatorial = {fatorial} é par");
+                }
+                else
+                {
+                    Console.WriteLine($"O fatorial = {fatorial} é impar");
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Soma até chegar no número/Soma.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soma_até_chegar_no_número
+{
+    internal class Soma
+    {
+        static void Main(string[] args)
+        {
+            /* 2) faça um programa que peça ao usuário um número
+            e some todas até esse número
+            EX: o usuário digitou 4 então a soma é (1+2+3+4=10)b */
+            float somando = 0;
+
+            // o número que o usuário digita é utilizado para delimitar a
+            // quantidade de repetições do laço for()
+            Console.WriteLine("Digite um número");
+            float num = float.Parse(Console.ReadLine());
+
+            for (float cont = 0; cont <= num; cont++)
+            {
+                somando += cont;
+            }
+            Console.WriteLine("Resultado {0}", somando);
+
+            Console.ReadKey();
+        }
+    }
+}
